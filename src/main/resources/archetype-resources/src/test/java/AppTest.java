e720eab3310b76b32162b88be0a93ef729860463
Rubem Mota <rubemmota89@gmail.com>
2025-06-12 17:53:01 +0100

    feat(openapi): Add basic project generation with openapi
    Added the possibility to generate a project with and without openapi support.

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+package $package;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
